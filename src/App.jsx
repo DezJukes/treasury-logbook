f18b1adc72a4c26fb867e9b707e8ca3e3a2b7b9b
IEMDomain04.dc <emmanman0011@gmail.com>
2025-07-15 20:36:21 +0800

    WILL NOT SCROLL-Y HAYUF NA YAN

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import SectionInputs from "./components/SectionInputs";
 import Navbar from "./components/Navbar";
 import { Toaster } from "sonner";
 
-
 function App() {
   return (
-    <>
+    <div className="h-screen overflow-hidden flex flex-col">
       <Toaster position="top-right" richColors />
       <Navbar />
-      <main className="min-h-screen flex justify-center items-start bg-gray-50 py-10">
+      <main className="flex justify-center items-start flex-1 bg-gray-50 overflow-y-auto py-10">
         <div className="max-sm:flex-col flex justify-center gap-10 w-full max-w-7xl px-6">
           <SectionInputs />
           <SectionDisplay />
         </div>
       </main>
-    </>
+    </div>
   );
 }
 

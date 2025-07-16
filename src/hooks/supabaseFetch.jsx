97cf0e7ae6991bb5cbd8241324827af42b86a108
DezJukes <jbryan08dc@gmail.com>
2025-07-16 10:41:14 +0800

    chore: fix to show current date

--- src/hooks/supabaseFetch.jsx
+++ src/hooks/supabaseFetch.jsx
@@ -3,10 +3,25 @@ import { supabase } from "@/createClient";
 
 export function useEntries() {
     const [entries, setEntries] = useState([]);
+    const today = new Date();
+
+    const startOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    0, 0, 0
+    ).toISOString();
+
+    const endOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    23, 59, 59, 999
+    ).toISOString();
 
     useEffect(() => {
         const fetchEntries = async () => {
-            const { data, error } = await supabase.from("visit_entries").select("*");
+            const { data, error } = await supabase.from("visit_entries").select("*").gte("date", startOfDay).lte("date", endOfDay);
             if (error) {
                 console.log("Error fetching: ", error);
             } else {

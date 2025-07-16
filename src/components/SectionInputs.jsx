2ac470e3a870c66b3ec39f40e6c857fbd597e7ff
DezJukes <jbryan08dc@gmail.com>
2025-07-16 10:40:45 +0800

    chore: passing data from createEntry

--- src/components/SectionInputs.jsx
+++ src/components/SectionInputs.jsx
@@ -12,8 +12,21 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { createEntry } from '@/hooks/createEntry';
 
 function SectionInputs() {
+  const {
+    studentNo,
+    setStudentNo,
+    studentName,
+    setStudentName,
+    purpose,
+    setPurpose,
+    staff,
+    setStaff,
+    handleSubmit,
+  } = createEntry();
+
   return (
     <main className="flex flex-col gap-5 max-w-md border border-gray-400 rounded-lg">
 
@@ -28,33 +41,39 @@ function SectionInputs() {
       {/* Student Number  */}
       <div className="flex flex-col gap-2 px-10">
         <Label>Student Number</Label>
-        <Input className="" placeholder="eg. 202234078" />
+        <Input className="" placeholder="eg. 202234078" 
+        onChange={(e) => setStudentNo(e.target.value)}
+        />
       </div>
 
       {/* Student Name  */}
       <div className="flex flex-col gap-2 px-10">
         <Label>Student Name</Label>
-        <Input className="" placeholder="Enter full name" />
+        <Input className="" placeholder="Enter full name" 
+        onChange={(e) => setStudentName(e.target.value)}
+        />
       </div>
 
       {/* Purpose  */}
       <div className="flex flex-col gap-2 px-10">
         <Label>Purpose of Visit</Label>
-        <Textarea placeholder="Describe the reason for the visit.." />
+        <Textarea placeholder="Describe the reason for the visit.." 
+        onChange={(e) => setPurpose(e.target.value)}
+        />
       </div>
 
       {/* Dropdown */}
       <div className="flex flex-col gap-2 px-10">
         <Label>Assigned Staff Member</Label>
-        <Select>
+        <Select value={staff} onValueChange={(value) => setStaff(value)}>
           <SelectTrigger className="w-full">
             <SelectValue/>
           </SelectTrigger>
           <SelectContent>
             <SelectItem>Select</SelectItem>
-            <SelectItem value="Cherry">Mam Cherry</SelectItem>
-            <SelectItem value="Larry">Sir Larry</SelectItem>
-            <SelectItem value="Sheila">Mam Sheila</SelectItem>
+            <SelectItem value="Mam Cherry">Mam Cherry</SelectItem>
+            <SelectItem value="Sir Larry">Sir Larry</SelectItem>
+            <SelectItem value="Mam Sheila">Mam Sheila</SelectItem>
           </SelectContent>
         </Select>
       </div>
@@ -62,7 +81,7 @@ function SectionInputs() {
       {/* Button */}
       {/* Bry, change mo lang itong onclick kapag ginawa mo na. Ayusin ko nalang yung user validation na yan. Pero try mo muna bago burahin para kita mo itsura. hahaha */}
       <div className="px-10 mb-5">
-        <Button onClick={() => {toast.success("Entry added successfully!");}} className="btn-entry bg-blue-700 w-full cursor-pointer"><CircleCheckBig />Add Entry</Button>
+        <Button onClick={handleSubmit} className="btn-entry bg-blue-700 w-full cursor-pointer"><CircleCheckBig />Add Entry</Button>
       </div>
     </main>
   );

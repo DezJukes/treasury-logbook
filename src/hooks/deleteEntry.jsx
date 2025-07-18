7d570b2a8c95638fc070515a6275ea365a04da29
IEMDomain04.dc <emmanman0011@gmail.com>
2025-07-18 15:31:12 +0800

    feat: add validation for the remove function

--- src/hooks/deleteEntry.jsx
+++ src/hooks/deleteEntry.jsx
@@ -1,10 +1,13 @@
 import { supabase } from "@/createClient";
+import { toast } from "sonner";
 
 export async function deleteEntry(id) {
   const { error } = await supabase.from("visit_entries").delete().eq("id", id);
-  if (error) {
-    console.log("Error deleting entry:", error);
+  if (error) 
+  {
+    toast.error("Failed to delete entry.", error);
     return false;
   }
-  return true;
+    toast.success("Entry deleted successfully.");
+    return true;
 }

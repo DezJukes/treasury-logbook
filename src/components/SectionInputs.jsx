bcda16f8f80a474c0c9eb72ba3b171dcc386c44d
IEMDomain04.dc <emmanman0011@gmail.com>
2025-07-18 15:00:44 +0800

    feat: add verification for the entry

--- src/components/SectionInputs.jsx
+++ src/components/SectionInputs.jsx
@@ -4,6 +4,16 @@ import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { toast } from "sonner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import React from "react";
 import {
   Select,
@@ -81,11 +91,26 @@ function SectionInputs() {
         </Select>
       </div>
 
-      {/* Button */}
-      {/* Bry, change mo lang itong onclick kapag ginawa mo na. Ayusin ko nalang yung user validation na yan. Pero try mo muna bago burahin para kita mo itsura. hahaha */}
-      <div className="px-10 mb-5">
-        <Button onClick={handleSubmit} className="btn-entry bg-blue-700 w-full cursor-pointer"><CircleCheckBig />Add Entry</Button>
-      </div>
+
+      <Dialog>
+        <div className="px-10 mb-5">
+          <DialogTrigger className="w-full"><Button className="btn-entry bg-blue-700 w-full cursor-pointer"><CircleCheckBig />Add Entry</Button></DialogTrigger>
+        </div>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogDescription>Double check you entry. We just keep tracking everyone who entered Treasury Office.</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button className="btn-confirm border border-gray-400" onClick={handleSubmit} variant="primary"><CircleCheckBig />Add Entry</Button>
+            </DialogClose>
+            <DialogClose asChild>
+              <Button className="btn-close" type="button" variant="secondary">Close</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }

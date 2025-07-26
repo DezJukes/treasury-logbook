f94aae0a8ddcef985ee535904d425434a853122e
IEMDomain04.dc <emmanman0011@gmail.com>
2025-07-26 21:09:24 +0800

    feat: nav responsive to mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,12 +24,12 @@ function Navbar() {
   };
 
   return (
-    <nav className="w-full flex justify-between items-center px-30 py-4 shadow-2xl bg-white border-b-4 border-yellow-300">
+    <nav className="w-full flex justify-between items-center px-5 md:px-30 py-4 shadow-2xl bg-white border-b-4 border-yellow-300">
       <div className="flex items-center gap-4">
-        <img className="h-20 w-20 rounded-md" src="/plm-logo.png" alt="plm-logo" />
+        <img className="h-auto w-10 md:w-20 rounded-md" src="/plm-logo.png" alt="plm-logo" />
         <div>
-          <h1 className="text-h2 font-bold text-yellow-600">Student Services Portal</h1>
-          <p className="text-text2 text-gray-500">Treasury Digital Logbook System</p>
+          <h1 className="text-text4 md:text-h2 font-bold text-yellow-600">Student Services Portal</h1>
+          <p className="text-text4 md:text-text2 text-gray-500">Treasury Digital Logbook System</p>
         </div>
       </div>
 

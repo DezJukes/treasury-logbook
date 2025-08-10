5c0d88f8f2860e3e1fc454a7f9a78df34d12ab57
DezJukes <jbryan08dc@gmail.com>
2025-08-10 13:51:43 +0800

    fix: model serializer

--- backend/logbook_backend/logbook/serializers.py
+++ backend/logbook_backend/logbook/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import VisitEntries
 
-class VisitEntriesSerializer(serializers.Serializer):
+class VisitEntriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = VisitEntries
         fields = [
